aa3ae0f855ae330a31ad64eb847655889384944e
FrancoFonsecaM <francos-fonsecam@unilibre.edu.co>
2025-11-15 10:46:07 -0500

    Add files via upload

---
+++ UPD_SQL_01.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO RPMTAB (TASEQ, TABCTR, TABDTR, TABFRA, TABTST, TABSTS, TABLBL, TABSES, TABFLG, TABFIL, TABRTY) VALUES
+(12,'RVMOP','Cargue reglas contables', '100', '01', 'A', '02', '/www/drpwdev1/htdocs/Cargue_Masivo/Response_file', '/www/drpwdev1/htdocs/Cargue_Masivo/Upload_File', '/www/drpwdev1/htdocs/Cargue_Masivo/Templates_file/TPL0201.csv', 'E');
+
+INSERT INTO RPMTAB (TABSEQ, TABDTR, TABTST, TABSTS, TABRTY) VALUES
+(2, 'RVMMO', 'Cargue reglas contables', '02', 'A', 'E');
